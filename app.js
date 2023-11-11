f8a19724195261fe1059a5f1a99a42a1b4739f77
maheshbasnet089 <basnetmanish089@gmail.com>
2023-11-11 17:34:54 +0545

    complete api

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // require express 
 const express = require("express")
+const { blogs } = require("./model/index")
 const app  = express()
 
 require("./model/index")
+// telling express to parse the incoming json data /
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.status(200).json({
@@ -10,6 +13,72 @@ app.get("/",(req,res)=>{
     })
 })
 
+// createBlog Api 
+app.post("/blogs",async (req,res)=>{
+    const title = req.body.title 
+    const subTitle = req.body.subTitle
+    const description = req.body.description 
+
+    // ALTERNATIVE 
+    // const {title,subTitle,description} = req.body
+
+    // insert the data to blogs table
+   await blogs.create({
+        title ,
+        subTitle,
+        description 
+    })
+    res.status(200).json({
+        message : "Blog created successfully"
+    })
+})
+
+// all Blogs Read API 
+app.get("/blogs",async (req,res)=>{
+  const data =  await blogs.findAll() // retuns array  [].length = 0 
+ if(data.length ==0){
+    res.status(404).json({
+        message : "No Blog found",
+        data 
+      })
+ }else{
+    res.status(200).json({
+        message : "Blogs fetched successfully",
+        data 
+      })
+ }
+
+})
+
+// single Blog Read API 
+app.get("/blogs/:id",async (req,res)=>{
+    const id = req.params.id
+    // const {id} = req.params 
+
+//    const data =  await blogs.findByPk(id)  // returns object ==> {} 
+const data = await blogs.findAll({
+    where : {
+        id : id
+    }
+}) // SELECT * FROM BLOGS WHERE id = id ==> returns array => []
+   res.status(200).json({
+    message : "Blog Fetched Successfully",
+    data
+   })
+})
+
+// delete blog api 
+app.delete("/blogs/:id",async(req,res)=>{
+    const id = req.params.id 
+   await blogs.destroy({
+        where : {
+            id : id
+        }
+    })
+    res.status(200).json({
+        message : "Blog deleted Successfully"
+    })
+})
 
 // listen to port
 
--- model/userModel.js
+++ model/userModel.js
@@ -1,16 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const Blog = sequelize.define("blog", {
-      title: {
+    const Blog = sequelize.define("user", {
+      userName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      subTitle: {
+      email: {
         type: DataTypes.STRING,
         allowNull : false
       },
-      description: {
+      password: {
         type: DataTypes.TEXT,
         allowNull:false
+      },
+      photo : {
+        type : DataTypes.STRING
       }
  
     });
